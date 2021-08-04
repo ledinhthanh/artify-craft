21d4cf0c89c1812e5a3d046dad937159e7e43e9d
SlowHand <WhiteShark1102@gmail.com>
2021-08-04 11:39:43 +0700

    Landing

---
+++ script.js
@@ -0,0 +1,24 @@
+var appName = "Stacy";
+var storeUrl = "https://1.envato.market/YgR1Xj";
+var date = "2021-08-04";
+
+function onIndexLoad() {
+  document.title = appName + " Flutter";
+  document.getElementById("storeUrl").href = storeUrl;
+}
+
+function onPolicyLoad() {
+  document.title = appName + " " + "Privacy Policy";
+  getData();
+}
+
+function onTermLoad() {
+  document.getElementById("appName2").innerHTML = appName;
+  document.title = appName + " " + "Terms and Conditions";
+  getData();
+}
+
+function getData() {
+  document.getElementById("appName").innerHTML = appName;
+  document.getElementById("date").innerHTML = date;
+}
